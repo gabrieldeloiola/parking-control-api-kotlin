45bffc73641d5dd69c2958e62924768480ab8567
Gabriel Loiola <>
2023-04-08 16:01:27 -0300

    Deleting the dto package. Changing the saveParkingSpot method in controller.

--- src/main/kotlin/br/com/api/gabrielpessoa/parkingcontrol/controllers/ParkingSpotController.kt
+++ src/main/kotlin/br/com/api/gabrielpessoa/parkingcontrol/controllers/ParkingSpotController.kt
@@ -1,10 +1,8 @@
 package br.com.api.gabrielpessoa.parkingcontrol.controllers
 
 import br.com.api.gabrielpessoa.parkingcontrol.domain.models.ParkingSpotModel
-import br.com.api.gabrielpessoa.parkingcontrol.dto.ParkingSpotDto
 import br.com.api.gabrielpessoa.parkingcontrol.services.ParkingSpotService
 import jakarta.validation.Valid
-import org.springframework.beans.BeanUtils
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 import java.time.ZoneId
-import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 
 @RestController
@@ -25,13 +22,9 @@ class ParkingSpotController(
 ) {
 
     @PostMapping
-    fun saveParkingSpot(@RequestBody @Valid parkingSpotDto: ParkingSpotDto): ResponseEntity<ParkingSpotModel> {
-        val parkingSpotModel = ParkingSpotModel()
-        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel)
+    fun saveParkingSpot(@RequestBody @Valid parkingSpotModel: ParkingSpotModel): ResponseEntity<ParkingSpotModel> {
         parkingSpotModel.registrationDate = LocalDateTime.now(ZoneId.of("UTC"))
         return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel)
-
-            //TODO - Criar o "save" no service.
         )
     }
 }
--- src/main/kotlin/br/com/api/gabrielpessoa/parkingcontrol/domain/models/ParkingSpotModel.kt
+++ src/main/kotlin/br/com/api/gabrielpessoa/parkingcontrol/domain/models/ParkingSpotModel.kt
@@ -1,56 +1,47 @@
 package br.com.api.gabrielpessoa.parkingcontrol.domain.models
 
 import jakarta.persistence.*
-import lombok.AllArgsConstructor
-import lombok.NoArgsConstructor
 import java.io.Serializable
 import java.time.LocalDateTime
-import java.util.*
+import java.util.UUID
 
 @Entity
-@Table(name = "TB_PARKING_SPOT")
-@NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "TB_PARKING_SPOT", schema = "PARKING_SPOT_SCHEMA")
 data class ParkingSpotModel(
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    val id: UUID,
+    val id: UUID = UUID.randomUUID(),
+
     @Column(nullable = false, unique = true, length = 10)
-    val parkingSpotNumber: String,
+    val parkingSpotNumber: String = "",
+
     @Column(nullable = false, unique = true, length = 7)
-    val licensePlateCar: String,
+    val licensePlateCar: String = "",
+
     @Column(nullable = false, length = 70)
-    val brandCar: String,
+    val brandCar: String = "",
+
     @Column(nullable = false, length = 70)
-    val modelCar: String,
+    val modelCar: String = "",
+
     @Column(nullable = false, length = 70)
-    val colorCar: String,
+    val colorCar: String = "",
+
     @Column(nullable = false)
-    var registrationDate: LocalDateTime,
+    var registrationDate: LocalDateTime = LocalDateTime.now(),
+
     @Column(nullable = false, length = 130)
-    val responsibleName: String,
+    val responsibleName: String = "",
+
     @Column(nullable = false, length = 30)
-    val apartment: String,
+    val apartment: String = "",
+
     @Column(nullable = false, length = 30)
-    val block: String
-
-) : Serializable {
-
-    private val serialVersionUID: Long = 1L
-
-    constructor() : this(
-        UUID.randomUUID(),
-        "",
-        "",
-        "",
-        "",
-        "",
-        LocalDateTime.now(),
-        "",
-        "",
-        ""
-    )
+    val block: String = "",
 
 
+    ) : Serializable {
+
+    val serialVersionUID: Long = 1
 }
